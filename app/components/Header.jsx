bf5f10523ba2357fcb1b8b3d22b20622714f9e42
mengchengma <mengchengma@gmail.com>
2025-12-11 14:11:42 -0500

    updated resume

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
             <a href='#contact' className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>
                 contact me <Image src={assets.right_arrow_white} alt='' width={16} height={16} className='w-4' />
             </a>
-            <a href={`${process.env.NODE_ENV === 'production' ? '/portfolio' : ''}/Meng Cheng Ma Resume.pdf`} download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>
+            <a href={`${process.env.NODE_ENV === 'production' ? '/portfolio' : ''}/MengChengMa_Resume.pdf`} download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>
                 my resume <Image src={assets.download_icon} alt='' width={16} height={16} className='w-4' />
             </a>
         </div>
